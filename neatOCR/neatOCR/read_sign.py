2ba6c84af29fd54c0f270fa801e1fc8330d1463c
cjhi <chilty@olin.edu>
2023-10-23 16:52:14 -0400

    Created Initial Skeleton Package named neatOCR

---
+++ neatOCR/neatOCR/read_sign.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from neatOCR.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ neatOCR/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'neatOCR'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='cjhi',
+    maintainer_email='chilty@olin.edu',
+    description='For the project located at https://github.com/cjhi/OCR',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'read_sign = neatOCR.read_sign:main'
+        ],
+    },
+)
