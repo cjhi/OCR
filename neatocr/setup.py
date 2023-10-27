4927ef2acc01a2ab4f0b77145e9cfd45dfeee1a6
laurent117 <lthorbecke11@gmail.com>
2023-10-27 14:30:52 -0400

    deleted easyocr package

--- neatocr/setup.py
+++ neatocr/setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-package_name = 'neatOCR'
+package_name = 'neatocr'
 
 setup(
     name=package_name,
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'read_sign = neatOCR.read_sign:main'
+            'read_sign = neatocr.read_sign:main'
         ],
     },
 )
--- test.py
+++ test.py
@@ -14,3 +14,8 @@ reader = easyocr.Reader(['ch_sim','en']) # this needs to run only once to load t
 #input = np.
 result = reader.readtext('turnRight.png')
 print(result)
+
+#consider putting signs in one color, pre-processing images to find text of interest before running OCR
+#frequency of checking for text will depend on latency of running OCR function - if 5 seconds, 1 image/five seconds, if 2s, 1 image/2 seconds
+
+#look at particle filter to see how you to move forward as soon as image updates7
